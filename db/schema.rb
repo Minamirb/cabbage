265a2b5ae097c7e78d89b9b8e5bc684e07cf73d9
satomicchy <satomi_t_sora@yahoo.co.jp>
2015-01-31 13:09:11 +0900

    add Town model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131040802) do
+ActiveRecord::Schema.define(version: 20150131040850) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -72,6 +72,15 @@ ActiveRecord::Schema.define(version: 20150131040802) do
   end
 
   add_index "shops", ["area_id"], name: "index_shops_on_area_id"
+
+  create_table "towns", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "towns", ["city_id"], name: "index_towns_on_city_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
